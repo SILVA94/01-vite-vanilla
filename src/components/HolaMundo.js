9f2e4ec7f51eebf153e93f297dcb76132267cbd0
Ricardo Silva <ric.silva15@gmail.com>
2023-05-31 05:39:59 -0600

    Primer commit

---
+++ src/components/HolaMundo.js
@@ -0,0 +1,6 @@
+const HolaMundo = () => `
+    <h1>Hola Mundo</h1>
+    <p>Este es un componente de prueba</p>
+`
+
+export default HolaMundo
---
+++ src/main.js
@@ -0,0 +1,12 @@
+import './styles/style.css'
+import HolaMundo from './components/HolaMundo'
+
+
+
+document.querySelector('#app').innerHTML = `
+   ${HolaMundo()}
+`
+
+
+
+
